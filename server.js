af43c3bbd16cb42dd4cb8905ce3b0204aaf161b1
Jacob Barton <jacobdbarton3@gmail.com>
2022-05-16 11:43:27 -0400

    installed mongoose connection

--- server.js
+++ server.js
@@ -4,16 +4,28 @@
 
 require("dotenv").config();
 
-const { PORT = 4000, MONGODB_URL } = process.env;
+const { PORT = 3000, MONGODB_URL } = process.env;
 
 const express = require("express");
 
 const app = express();
 
+const mongoose = require("mongoose");
+
+///////////////////////////////
+// DATABASE CONNECTION
+////////////////////////////////
+
+mongoose.connect(MONGODB_URL);
+
+mongoose.connection
+  .on("open", () => console.log("Your are connected to mongoose"))
+  .on("close", () => console.log("Your are disconnected from mongoose"))
+  .on("error", (error) => console.log(error));
+
 ///////////////////////////////
 // ROUTES
 ////////////////////////////////
-
 
 app.get("/", (req, res) => {
   res.send("hello world");
@@ -22,5 +34,4 @@ app.get("/", (req, res) => {
 ///////////////////////////////
 // LISTENER
 ////////////////////////////////
-
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
